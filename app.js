3bb71f3eb52c6c0a3360d58eb04b567d057f1356
Mohammed Mulazada <thisismoniac@gmail.com>
2018-07-13 16:41:21 +0200

    added eslint and stylelint checking

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const ENV = require( 'dotenv' ).config().parsed
+const bodyParser = require( 'body-parser' )
+const app = express()
+const PORT = ENV.NODE_ENV === 'production' ? ENV.PORT : ENV.DEV_PORT
+
+app.set( 'views', './views' )
+app.set( 'view engine', 'ejs' )
+
+app.use( bodyParser.urlencoded( {
+	extended: false
+} ) )
+app.use( bodyParser.json() )
+
+app.listen( PORT, () => {
+
+	console.log( `Hello from http://localhost:${ PORT }` )
+
+} )
