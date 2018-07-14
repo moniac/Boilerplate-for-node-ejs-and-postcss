555ea828011338ac0f3002756ceaf022725fd8c1
Mohammed Mulazada <thisismoniac@gmail.com>
2018-07-14 16:15:12 +0200

    Added reset

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ const express = require( 'express' )
 const ENV = require( 'dotenv' ).config().parsed
 const bodyParser = require( 'body-parser' )
 const app = express()
+const fs = require( 'fs' )
 const PORT = ENV.NODE_ENV === 'production' ? ENV.PORT : ENV.DEV_PORT
 
 app.set( 'views', './views' )
 app.set( 'view engine', 'ejs' )
+
+app.get( '*', ( req, res, next ) => {
+
+	const CSS_FILE = './public/styles/critical.css'
+	fs.readFile( CSS_FILE, 'utf8', ( err,data ) => {
+		if ( err ) {
+			return console.log( err )
+		}
+		if ( data ) {
+			app.locals.criticalCSS = data
+		}
+
+		next()
+	} )
+
+} )
 
 app.use( express.static( 'public' ) )
 app.use( express.static( 'views' ) )
